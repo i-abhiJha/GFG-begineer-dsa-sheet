c02a1f7b6705f3823a0125b1ee118b0991296a70
Abhinav Kumar Jha <62744709+i-abhiJha@users.noreply.github.com>
2022-07-10 10:55:30 +0530

    Add files via upload

---
+++ search and element in an array.cpp
@@ -0,0 +1,51 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        int ans=-1;
+    for(int i=0;i<N;i++){
+        if(arr[i]==X){
+            ans=i;
+            break;
+        }
+    }
+    return ans;
+        
+    }
+
+};
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+    
+}
